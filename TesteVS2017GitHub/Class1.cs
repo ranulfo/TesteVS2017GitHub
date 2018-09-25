985a7244c046119e022795189c3102a0153d5ba6
ranulfo <ranulfooliveira@gmail.com>
2018-09-25 09:09:21 -0300

    Add project files.

---
+++ TesteVS2017GitHub/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteVS2017GitHub
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura)
+        {
+            return 0;
+        }
+
+        public static double FahrenheitParaKelvin(double temperatura)
+        {
+            return 0;
+        }
+    }
+}
